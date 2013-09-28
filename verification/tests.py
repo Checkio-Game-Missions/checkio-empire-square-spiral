00fe311cdefd0800fee978d9505567dcf5559bcb
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-09-28 15:00:12 +1200

    referee and tests

--- verification/tests.py
+++ verification/tests.py
@@ -7,30 +7,246 @@ Each test is dict with
     "explanation" -- not necessary key, it's using for additional info in animation.
 """
 
-
 TESTS = {
     "Basics": [
         {
-            "input": [3, 2],
-            "answer": 5,
-            "explanation": "3+2=?"
+            "input": [1, 9],
+            "answer": 2
+        },
+        {
+            "input": [9, 1],
+            "answer": 2
+        },
+        {
+            "input": [10, 25],
+            "answer": 1
+        },
+        {
+            "input": [5, 9],
+            "answer": 4
+        },
+        {
+            "input": [26, 31],
+            "answer": 5
+        },
+        {
+            "input": [50, 16],
+            "answer": 10
+        },
+        {
+            "input": [10, 3],
+            "answer": 3
+        },
+        {
+            "input": [4, 1],
+            "answer": 1
+        },
+        {
+            "input": [1, 6],
+            "answer": 1
+        },
+        {
+            "input": [9, 2],
+            "answer": 1
+        },
+        {
+            "input": [96, 5],
+            "answer": 5
+        },
+        {
+            "input": [59, 15],
+            "answer": 4
+        },
+        {
+            "input": [49, 53],
+            "answer": 4
+        },
+        {
+            "input": [54, 46],
+            "answer": 8
+        },
+        {
+            "input": [17, 3],
+            "answer": 4
+        },
+        {
+            "input": [7, 8],
+            "answer": 1
+        },
+        {
+            "input": [9, 4],
+            "answer": 3
         },
         {
             "input": [5, 7],
-            "answer": 12,
-            "explanation": "5+7=?"
+            "answer": 2
+        },
+        {
+            "input": [9, 6],
+            "answer": 3
+        },
+        {
+            "input": [4, 2],
+            "answer": 2
+        },
+        {
+            "input": [55, 44],
+            "answer": 11
+        },
+        {
+            "input": [98, 67],
+            "answer": 5
+        },
+        {
+            "input": [90, 1],
+            "answer": 9
+        },
+        {
+            "input": [39, 1],
+            "answer": 4
+        },
+        {
+            "input": [49, 40],
+            "answer": 9
+        },
+        {
+            "input": [460, 200],
+            "answer": 22
+        },
+        {
+            "input": [820, 252],
+            "answer": 26
         }
     ],
     "Extra": [
         {
-            "input": [6, 3],
-            "answer": 9,
-            "explanation": "6+3=?"
+            "input": [8, 3],
+            "answer": 3
         },
         {
-            "input": [6, 7],
-            "answer": 13,
-            "explanation": "6+7=?"
+            "input": [7, 10],
+            "answer": 3
+        },
+        {
+            "input": [5, 1],
+            "answer": 2
+        },
+        {
+            "input": [10, 9],
+            "answer": 1
+        },
+        {
+            "input": [5, 2],
+            "answer": 3
+        },
+        {
+            "input": [25, 85],
+            "answer": 4
+        },
+        {
+            "input": [29, 69],
+            "answer": 8
+        },
+        {
+            "input": [51, 66],
+            "answer": 13
+        },
+        {
+            "input": [14, 21],
+            "answer": 7
+        },
+        {
+            "input": [4, 93],
+            "answer": 7
+        },
+        {
+            "input": [4, 8],
+            "answer": 2
+        },
+        {
+            "input": [6, 3],
+            "answer": 3
+        },
+        {
+            "input": [10, 8],
+            "answer": 2
+        },
+        {
+            "input": [8, 3],
+            "answer": 3
+        },
+        {
+            "input": [44, 49],
+            "answer": 5
+        },
+        {
+            "input": [14, 89],
+            "answer": 5
+        },
+        {
+            "input": [55, 21],
+            "answer": 10
+        },
+        {
+            "input": [9, 95],
+            "answer": 6
+        },
+        {
+            "input": [25, 19],
+            "answer": 6
+        },
+        {
+            "input": [7, 1],
+            "answer": 2
+        },
+        {
+            "input": [4, 6],
+            "answer": 2
+        },
+        {
+            "input": [4, 7],
+            "answer": 3
+        },
+        {
+            "input": [6, 5],
+            "answer": 1
+        },
+        {
+            "input": [8, 5],
+            "answer": 3
+        },
+        {
+            "input": [44, 2],
+            "answer": 6
+        },
+        {
+            "input": [65, 19],
+            "answer": 6
+        },
+        {
+            "input": [47, 62],
+            "answer": 9
+        },
+        {
+            "input": [89, 64],
+            "answer": 9
+        },
+        {
+            "input": [41, 5],
+            "answer": 4
+        },
+        {
+            "input": [143, 46],
+            "answer": 13
+        },
+        {
+            "input": [775, 23],
+            "answer": 20
+        },
+        {
+            "input": [429, 775],
+            "answer": 26
         }
+
     ]
 }
