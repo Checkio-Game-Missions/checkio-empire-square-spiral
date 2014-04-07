49e44fd1ff2cc673fabccff54a9c527b482fc064
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-04-07 16:21:14 +1200

    new tests

--- verification/tests.py
+++ verification/tests.py
@@ -33,220 +33,66 @@ TESTS = {
             "input": [50, 16],
             "answer": 10
         },
+    ],
+    "Edge": [
         {
-            "input": [10, 3],
-            "answer": 3
-        },
-        {
-            "input": [4, 1],
+            "input": [1, 2],
             "answer": 1
         },
         {
-            "input": [1, 6],
-            "answer": 1
-        },
-        {
-            "input": [9, 2],
-            "answer": 1
-        },
-        {
-            "input": [96, 5],
-            "answer": 5
-        },
-        {
-            "input": [59, 15],
-            "answer": 4
-        },
-        {
-            "input": [49, 53],
-            "answer": 4
-        },
-        {
-            "input": [54, 46],
+            "input": [99, 1],
             "answer": 8
         },
         {
-            "input": [17, 3],
-            "answer": 4
+            "input": [999, 1],
+            "answer": 26
         },
         {
-            "input": [7, 8],
+            "input": [998, 999],
             "answer": 1
         },
         {
-            "input": [9, 4],
-            "answer": 3
+            "input": [73, 91],
+            "answer": 18
         },
         {
-            "input": [5, 7],
-            "answer": 2
+            "input": [100, 82],
+            "answer": 18
         },
         {
-            "input": [9, 6],
-            "answer": 3
+            "input": [900, 961],
+            "answer": 59
         },
         {
-            "input": [4, 2],
-            "answer": 2
-        },
-        {
-            "input": [55, 44],
-            "answer": 11
-        },
-        {
-            "input": [98, 67],
-            "answer": 5
-        },
-        {
-            "input": [90, 1],
+            "input": [86, 69],
             "answer": 9
         },
-        {
-            "input": [39, 1],
-            "answer": 4
-        },
-        {
-            "input": [49, 40],
-            "answer": 9
-        },
-        {
-            "input": [460, 200],
-            "answer": 22
-        },
-        {
-            "input": [820, 252],
-            "answer": 26
-        }
+
     ],
     "Extra": [
         {
-            "input": [8, 3],
-            "answer": 3
-        },
-        {
-            "input": [7, 10],
-            "answer": 3
-        },
-        {
-            "input": [5, 1],
-            "answer": 2
-        },
-        {
-            "input": [10, 9],
-            "answer": 1
-        },
-        {
-            "input": [5, 2],
-            "answer": 3
-        },
-        {
-            "input": [25, 85],
+            "input": [2, 18],
             "answer": 4
         },
         {
-            "input": [29, 69],
-            "answer": 8
+            "input": [777, 555],
+            "answer": 18
         },
         {
-            "input": [51, 66],
-            "answer": 13
+            "input": [100, 10],
+            "answer": 12
         },
         {
-            "input": [14, 21],
-            "answer": 7
-        },
-        {
-            "input": [4, 93],
-            "answer": 7
-        },
-        {
-            "input": [4, 8],
-            "answer": 2
-        },
-        {
-            "input": [6, 3],
-            "answer": 3
-        },
-        {
-            "input": [10, 8],
-            "answer": 2
-        },
-        {
-            "input": [8, 3],
-            "answer": 3
-        },
-        {
-            "input": [44, 49],
-            "answer": 5
-        },
-        {
-            "input": [14, 89],
-            "answer": 5
-        },
-        {
-            "input": [55, 21],
-            "answer": 10
-        },
-        {
-            "input": [9, 95],
-            "answer": 6
-        },
-        {
-            "input": [25, 19],
-            "answer": 6
-        },
-        {
-            "input": [7, 1],
-            "answer": 2
-        },
-        {
-            "input": [4, 6],
-            "answer": 2
-        },
-        {
-            "input": [4, 7],
-            "answer": 3
-        },
-        {
-            "input": [6, 5],
-            "answer": 1
-        },
-        {
-            "input": [8, 5],
-            "answer": 3
-        },
-        {
-            "input": [44, 2],
-            "answer": 6
-        },
-        {
-            "input": [65, 19],
-            "answer": 6
-        },
-        {
-            "input": [47, 62],
+            "input": [69, 96],
             "answer": 9
         },
         {
-            "input": [89, 64],
-            "answer": 9
-        },
-        {
-            "input": [41, 5],
-            "answer": 4
-        },
-        {
-            "input": [143, 46],
+            "input": [521, 2],
             "answer": 13
         },
         {
-            "input": [775, 23],
-            "answer": 20
+            "input": [81, 65],
+            "answer": 16
         },
-        {
-            "input": [429, 775],
-            "answer": 26
-        }
-
     ]
 }
