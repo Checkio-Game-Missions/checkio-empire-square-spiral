fd61ea4897e98b390583d0c5708af7d3a00d0979
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-04-07 15:47:59 +1200

    update

--- verification/referee.py
+++ verification/referee.py
@@ -1,8 +1,15 @@
 from checkio.signals import ON_CONNECT
 from checkio import api
 from checkio.referees.io import CheckiOReferee
+from checkio.referees.cover_codes import unwrap_args
 from tests import TESTS
 
 api.add_listener(
     ON_CONNECT,
-    CheckiOReferee(tests=TESTS).on_ready)
+    CheckiOReferee(tests=TESTS,
+                   cover_code={
+                       'python-27': unwrap_args,  # or None
+                       'python-3': unwrap_args
+                   },
+                   function_name='find_distance'
+    ).on_ready)
