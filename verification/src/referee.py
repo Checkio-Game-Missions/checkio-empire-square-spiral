49061ff59e3b27906330d03db9bb133f095766e0
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-03-18 12:00:37 +0300

    add repr

--- verification/src/referee.py
+++ verification/src/referee.py
@@ -1,21 +1,25 @@
 from checkio_referee import RefereeCodeGolf
-from checkio_referee.covercode import py_unwrap_args
+from checkio_referee import covercodes, representations
 
 import settings
 import settings_env
 from tests import TESTS
 
 
-
 class Referee(RefereeCodeGolf):
     TESTS = TESTS
-    DEFAULT_LENGTH = 250
+    DEFAULT_MAX_CODE_LENGTH = 250
     BASE_POINTS = 25
     EXECUTABLE_PATH = settings.EXECUTABLE_PATH
     CURRENT_ENV = settings_env.CURRENT_ENV
     FUNCTION_NAME = "find_distance"
     ENV_COVERCODE = {
-        "python_2": py_unwrap_args,
-        "python_3": py_unwrap_args,
+        "python_2": covercodes.py_unwrap_args,
+        "python_3": covercodes.py_unwrap_args,
         "javascript": None
+    }
+    CALLED_REPRESENTATIONS = {
+        "python_2": representations.unwrap_arg_representation,
+        "python_3": representations.unwrap_arg_representation,
+        "javascript": representations.unwrap_arg_representation,
     }
